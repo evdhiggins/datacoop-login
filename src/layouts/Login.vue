2f0b9294426398511d8dbe0b88432a42edf21c39
Evan Higgins <evdhiggins@gmail.com>
2018-04-14 09:31:04 -0500

    Remove unused .data & .computed, adjusted layout

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -6,41 +6,24 @@ v-container(fluid fill-height)
         v-toolbar(dark color="primary")
           v-toolbar-title
             slot(name="title") Login to DataCoop
-          v-spacer
+
         v-card-text
           slot(name="body")
+
           slot(name="footer")
             v-spacer
             router-link(:to="{name: 'ForgotPassword'}") I forgot my password
             v-spacer
             router-link(:to="{name: 'VerifyAccount'}") I have a password reset code
+
         v-card-actions
           v-spacer
           slot(name="actions")
+
 </template>
 
 <script>
 export default {
-  name: "LoginLayout",
-  props: {
-    value: {
-      required: false,
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {};
-  },
-  computed: {
-    validForm: {
-      set(value) {
-        this.$emit("input", value);
-      },
-      get() {
-        return this.value;
-      }
-    }
-  }
+  name: "LoginLayout"
 };
 </script>
